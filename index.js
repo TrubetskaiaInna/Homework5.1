b4742da812a2419bea99a959e6b871c9eb8c2b45
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-07 11:29:41 +0200

    created isPrime function

---
+++ index.js
@@ -0,0 +1,20 @@
+const isPrime = (n) => {
+  if (n === 0 || n === 1) {
+    return false
+  } else {
+    let arr = []
+    const steps = Math.round(Math.sqrt(n))
+    for (let i = 2; i <= steps; i++) {
+      arr.push(n % i)
+    }
+    let resultIsPrime = true
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === 0) {
+        resultIsPrime = false
+        break
+      }
+    }
+    return resultIsPrime
+  }
+}
+console.log(isPrime(13))
